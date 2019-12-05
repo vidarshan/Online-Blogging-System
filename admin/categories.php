a9c9d09fee2685bc9eca75fb5a3ad6562abb9fb6
vidarshanadithya98 <vidarshanadithya3@gmail.com>
2019-12-05 23:22:55 +0530

    Displaying categories from DB

--- admin/categories.php
+++ admin/categories.php
@@ -1,7 +1,7 @@
-<?php include "includes/header.php"; ?>
+<?php include "includes/admin_header.php"; ?>
     <div id="wrapper">
 
-    <?php include "includes/navigation.php"; ?>
+    <?php include "includes/admin_navigation.php"; ?>
 
 
 
@@ -31,6 +31,15 @@
                         </form>
                         </div>
 
+                        
+                <?php
+                
+                        $query = "SELECT * FROM categories";
+
+                        $select_categories = mysqli_query($connection, $query);
+                ?>
+
+
                         <div class="col-xs-6">
                             <table class="table table-bordered table-hover">
                                 <thead>
@@ -39,10 +48,22 @@
                                         <th>Category Title</th>
                                     </tr>
                                     <tbody>
-                                        <tr>
-                                            <td>Baseball Category</td>
-                                            <td>BasketBall Category</td>
-                                        </tr>
+                                       
+                                        <?php
+                                                while($row = mysqli_fetch_assoc($select_categories)){
+
+                                                    $cat_id = $row['cat_id'];
+                                                    $cat_title = $row['cat_title'];
+
+                                                    echo "<tr>";
+                                                    echo "<td>${cat_id}</td>";
+                                                    echo "<td>${cat_title}</td>";
+                                                    echo "</tr>"; 
+
+                                                } 
+
+                                        ?> 
+                                        
                                     </tbody>
                                 </thead>
                             </table>
@@ -62,4 +83,4 @@
 
 
 
-        <?php include "includes/footer.php"; ?>
+        <?php include "includes/admin_footer.php"; ?>
--- admin/index.php
+++ admin/index.php
@@ -1,11 +1,11 @@
-<?php include "includes/header.php"; ?>
+<?php include "includes/admin_header.php"; ?>
     <div id="wrapper">
 
 
 
 
 
-    <?php include "includes/navigation.php"; ?>
+    <?php include "includes/admin_navigation.php"; ?>
 
 
 
@@ -38,4 +38,4 @@
 
 
 
-        <?php include "includes/footer.php"; ?>
+        <?php include "includes/admin_footer.php"; ?>
