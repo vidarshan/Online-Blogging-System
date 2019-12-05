1f79c7c2b8bb192aff29ef8b4c138f6dc4f937d5
vidarshanadithya98 <vidarshanadithya3@gmail.com>
2019-12-05 23:15:20 +0530

    Added categories table

--- admin/categories.php
+++ admin/categories.php
@@ -22,13 +22,30 @@
                         <div class="col-xs-6">
                         <form action="">
                             <div class="form-group">
-                            <label for="">Category Title</label>
+                            <label for="">Category name : </label>
                             <input class="form-control" type="text" name="cat_title">
                             </div>
                             <div class="form-group">
                             <input class="btn btn-warning" type="submit" name="submit" value="Add Category">
                             </div>
                         </form>
+                        </div>
+
+                        <div class="col-xs-6">
+                            <table class="table table-bordered table-hover">
+                                <thead>
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>Category Title</th>
+                                    </tr>
+                                    <tbody>
+                                        <tr>
+                                            <td>Baseball Category</td>
+                                            <td>BasketBall Category</td>
+                                        </tr>
+                                    </tbody>
+                                </thead>
+                            </table>
                         </div>
 
 
