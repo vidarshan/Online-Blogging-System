13d7ec09b1e134e6e55650fd5498dece007c4b75
vidarshanadithya98 <vidarshanadithya3@gmail.com>
2019-12-05 21:30:16 +0530

    Custom search engine part 2

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -6,7 +6,20 @@
                    if(isset($_POST['submit'])){
 
                         $search = $_POST['search'];
-                        
+                        $query = "SELECT * FROM posts WHERE post_tags LIKE '%$search%' ";
+                        $search_query = mysqli_query($connection, $query);
+
+                        if(!$search_query){
+                            die("Query failed " . mysqli_error($connection));
+                        }
+
+                        $count = mysqli_num_rows($search_query);
+
+                        if($count == 0){
+                            echo "<h1>No result</h1>";
+                        }else{
+                            echo "Some result";
+                        }
                    }
                 ?>
                 <!-- Blog Search Well -->
