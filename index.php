ed44c3a85051049354c406ae77b54f70d98ecb31
vidarshanadithya98 <vidarshanadithya3@gmail.com>
2019-12-05 21:04:29 +0530

    Displaying posts from database

--- index.php
+++ index.php
@@ -11,26 +11,48 @@
             <!-- Blog Entries Column -->
             <div class="col-md-8">
 
-                <h1 class="page-header">
-                    Page Heading
-                    <small>Secondary Text</small>
-                </h1>
+            <?php
 
-                <!-- First Blog Post -->
-                <h2>
-                    <a href="#">Blog Post Title</a>
-                </h2>
-                <p class="lead">
-                    by <a href="index.php">Start Bootstrap</a>
-                </p>
-                <p><span class="glyphicon glyphicon-time"></span> Posted on August 28, 2013 at 10:00 PM</p>
-                <hr>
-                <img class="img-responsive" src="http://placehold.it/900x300" alt="">
-                <hr>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, veritatis, tempora, necessitatibus inventore nisi quam quia repellat ut tempore laborum possimus eum dicta id animi corrupti debitis ipsum officiis rerum.</p>
-                <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+                $query = "SELECT * FROM posts";
 
-                <hr>
+                $select_all_posts_query = mysqli_query($connection, $query);
+
+                while($row = mysqli_fetch_assoc($select_all_posts_query)){
+
+                    $post_title = $row['post_title'];
+                    $post_author = $row['post_author'];
+                    $post_image = $row['post_image'];
+                    $post_date = $row['post_date'];
+                    $post_content = $row['post_content'];
+                    
+                    ?>
+
+                        <h1 class="page-header">
+                        Page Heading
+                        <small>Secondary Text</small>
+                        </h1>
+
+                        <!-- First Blog Post -->
+                        <h2>
+                        <a href="#"><?php echo $post_title; ?></a>
+                        </h2>
+                        <p class="lead">
+                        by <a href="index.php"><?php echo $post_author; ?></a>
+                        </p>
+                        <p><span class="glyphicon glyphicon-time"></span> <?php echo $post_date; ?> </p>
+                        <hr>
+                        <img class="img-responsive" src="http://placehold.it/900x300" alt="">
+                        <hr>
+                        <p><?php echo $post_content; ?></p>
+                        <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+
+                        <hr>
+
+                        <?php
+                      
+                }  
+            ?>
+
 
 
 
@@ -48,3 +70,6 @@
         <hr>
 
         <?php include "includes/footer.php"; ?>
+
+
+       
