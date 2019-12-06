19c6c84752fe485f9eba9917efc01ce0fb6e3a7b
vidarshanadithya98 <vidarshanadithya3@gmail.com>
2019-12-06 17:40:05 +0530

    Added post table in admin

---
+++ admin/posts.php
@@ -0,0 +1,63 @@
+<?php include "includes/admin_header.php"; ?>
+
+
+    <div id="wrapper">
+
+        <!-- Navigation -->
+        <?php include "includes/admin_navigation.php"; ?>
+
+        <div id="page-wrapper">
+
+            <div class="container-fluid">
+
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+                    <h1 class="page-header">
+                            Welcome to admin page
+                            <small>Author</small>
+                        </h1>
+
+
+                        <table class="table table-bordered table-hover">
+                            <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Post Author</th>
+                                    <th>Post Title</th>
+                                    <th>Post Category</th>
+                                    <th>Post Status</th>
+                                    <th>Post Image</th>
+                                    <th>Post Tags</th>
+                                    <th>Comments</th>
+                                    <th>Date</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>Default Data</td>
+                                    <td>Default Data</td>
+                                    <td>Default Data</td>
+                                    <td>Default Data</td>
+                                    <td>Default Data</td>
+                                    <td>Default Data</td>
+                                    <td>Default Data</td>
+                                    <td>Default Data</td>
+                                    <td>Default Data</td>
+                                </tr>
+                            </tbody>
+                        </table>
+
+
+
+
+                    </div>
+                </div>
+                <!-- /.row -->
+
+            </div>
+            <!-- /.container-fluid -->
+
+        </div>
+        <!-- /#page-wrapper -->
+<?php include "includes/admin_footer.php"; ?>
