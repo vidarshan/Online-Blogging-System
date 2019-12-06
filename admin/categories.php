fa270b0e2e7c039251e9472105eb0d173b333145
vidarshanadithya98 <vidarshanadithya3@gmail.com>
2019-12-06 17:26:08 +0530

    Refactoring code part 1

--- admin/categories.php
+++ admin/categories.php
@@ -1,5 +1,6 @@
 <?php include "includes/admin_header.php"; ?>
 
+
     <div id="wrapper">
 
         <!-- Navigation -->
@@ -20,20 +21,7 @@
                         <div class="col-xs-6">
 
                         <?php
-                            if(isset($_POST['submit'])){
-                                
-                              $cat_title = $_POST['cat_title'];
-                              if($cat_title == "" || empty($cat_title)){
-                                echo "This field should not be empty";
-                              }else{
-                                $query = "INSERT INTO categories(cat_title) ";
-                                $query .= "VALUE('{$cat_title}') ";
-                                $create_category_query = mysqli_query($connection, $query);
-                                if(!$create_category_query){
-                                        die("Query failed ". mysqli_error($connection));
-                                }
-                              }
-                            }
+                                insert_categories();
                         ?>
 
                             <form action="" method="post">
