2eee03b63af4b4bbb7b8bc0cdafcfa54716d1025
vidarshanadithya98 <vidarshanadithya3@gmail.com>
2019-12-06 23:04:18 +0530

    Added category dropdown

--- admin/includes/add_post.php
+++ admin/includes/add_post.php
@@ -3,7 +3,7 @@
 
         $post_title = $_POST['title'];
         $post_author = $_POST['author'];
-        $post_category_id = $_POST['post_category_id'];
+        $post_category_id = $_POST['post_category'];
         $post_status = $_POST['post_status'];
 
 
@@ -48,9 +48,23 @@
 
     <div class="form-group">
     
-        <label for="post_category">Post Category</label>
-        <input type="text" class="form-control" name="post_category_id">
-            
+   <select name="post_category" id="post_category">
+            <?php
+
+                $query = "SELECT * FROM categories";
+                $select_categories = mysqli_query($connection, $query);
+
+                confirmQuery($select_categories);
+                while($row = mysqli_fetch_assoc($select_categories)){
+                $cat_id = $row['cat_id'];
+                $cat_title = $row['cat_title'];
+
+
+                echo "<option value='${cat_id}'>${cat_title}</option>";
+                }
+
+            ?>
+        </select>
     </div>
 
     <div class="form-group">
@@ -96,3 +110,4 @@
             
     </div>
 </form>
+
