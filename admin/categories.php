bd802a14ac4fca8b2336bb5bb0607355cb41efa6
vidarshanadithya98 <vidarshanadithya3@gmail.com>
2019-12-06 17:30:34 +0530

    Refactoring code part 2

--- admin/categories.php
+++ admin/categories.php
@@ -67,31 +67,12 @@
                             <tbody>
 
                             <?php
-                                $query = "SELECT * FROM categories";
-                                $select_categories = mysqli_query($connection, $query);
-                                    while($row = mysqli_fetch_assoc($select_categories)){
-                                    $cat_id = $row['cat_id'];
-                                    $cat_title = $row['cat_title'];
-                                    echo "<tr>";
-                                    echo "<td>{$cat_id}</td>";
-                                    echo "<td>{$cat_title}</td>";
-                                    echo "<td><a href='categories.php?delete={$cat_id}'>DELETE</a></td>";
-                                    echo "<td><a href='categories.php?edit={$cat_id}'>EDIT</a></td>";
-                                    echo "<tr>";
-                                }
+                                    findAllCategories();
                             ?>     
                             
                                 <?php
                                     
-                                    if(isset($_GET['delete'])){
-                                        $the_cat_id = $_GET['delete'];
-                                        
-                                    $query = "DELETE FROM categories WHERE cat_id = {$the_cat_id} ";
-                                    $delete_query = mysqli_query($connection, $query);
-                                       
-                                    header("Location: categories.php");
-                                    
-                                    }
+                                    deleteCategories();
                                 ?>
                              </tbody>
                         </table>
