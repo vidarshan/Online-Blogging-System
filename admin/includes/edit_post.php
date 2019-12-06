e69ad5ddff71847f5bad31433c732f9da18def00
vidarshanadithya98 <vidarshanadithya3@gmail.com>
2019-12-06 22:44:41 +0530

    Updating posts function added

--- admin/includes/edit_post.php
+++ admin/includes/edit_post.php
@@ -8,7 +8,7 @@
 
 
 
-        $query = "SELECT * FROM posts";
+$query = "SELECT * FROM posts WHERE post_id = {$the_post_id} ";
         $select_posts_by_id = mysqli_query($connection, $query);
             while($row = mysqli_fetch_assoc($select_posts_by_id)){
             $post_id = $row['post_id'];
@@ -24,15 +24,64 @@
             
 
         }
+
+        if(isset($_POST['update_post'])){
+
+            $post_title = $_POST['title'];
+            $post_author = $_POST['author'];
+            $post_category_id = $_POST['post_category'];
+            $post_status = $_POST['post_status'];
+    
+    
+    
+            //use super global $_FILES
+            $post_image = $_FILES['image']['name'];
+            $post_image_temp = $_FILES['image']['tmp_name'];
+    
+    
+    
+            $post_tags = $_POST['post_tags'];
+            $post_content = $_POST['post_content'];
+
+
+            move_uploaded_file($post_image_temp, "../images/$post_image" );
+        
+            if(empty($post_image)){
+
+                $query = "SELECT * FROM posts WHERE post_id = $the_post_id ";
+                $select_image = mysqli_query($connection, $query);
+
+                while($row = mysqli_fetch_array($select_image)){
+                    $post_image = $row['post_image'];
+                }       
+            }
+                
+        
+            $query = "UPDATE posts SET ";
+            /////////////////////////////////////////
+            $query .= "post_category_id = '{$post_category_id}', ";
+            $query .= "post_title = '{$post_title}', ";
+            $query .= "post_author = '{$post_author}', ";
+            $query .= "post_date = now(), ";
+            $query .= "post_image = '{$post_image}', ";
+            $query .= "post_content = '{$post_content}', ";
+            $query .= "post_tags = '{$post_tags}', ";
+            $query .= "post_status = '{$post_status}' ";
+            $query .= "WHERE post_id = {$the_post_id} ";
+
+
+            $update_post = mysqli_query($connection, $query);
+
+
+            confirmQuery($update_post);
+
+            
+        }
     
 
 
 
 ?>
-
-
-
-
 
 
 <form action="" method="post" enctype="multipart/form-data">
@@ -84,6 +133,7 @@
 <div class="form-group">
     
         <img width="100" src="../images/<?php echo $post_image; ?>" alt="">
+        <input type="file" name="image">
         
 </div>
 
@@ -105,7 +155,7 @@
 
 <div class="form-group">
     
-<input type="submit" value="Publish post" name="create_post" class="btn btn-primary ">
+<input type="submit" value="Publish post" name="update_post" class="btn btn-primary ">
         
 </div>
 </form>
