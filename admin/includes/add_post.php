effdc248c25677a15096d5bf3985a2a577925813
vidarshanadithya98 <vidarshanadithya3@gmail.com>
2019-12-07 18:42:42 +0530

    Increasing post comment count

--- admin/includes/add_post.php
+++ admin/includes/add_post.php
@@ -17,7 +17,7 @@
         $post_tags = $_POST['post_tags'];
         $post_content = $_POST['post_content'];
         $post_date = date('d-m-y');
-        $post_comment_count = 4;
+        //$post_comment_count = 4;
 
 
         //functions for images
@@ -29,7 +29,7 @@
 
         //above errors occur when an text is inserted to int column.
         $query = "INSERT INTO posts(post_category_id,post_title,post_author,post_date,post_image,post_content,post_tags,post_comment_count,post_status) ";
-        $query .= "VALUES({$post_category_id},'{$post_title}','{$post_author}',now(),'{$post_image}','{$post_content}','{$post_tags}','{$post_comment_count}','{$post_status}' ) ";
+        $query .= "VALUES({$post_category_id},'{$post_title}','{$post_author}',now(),'{$post_image}','{$post_content}','{$post_tags}','{$post_status}' ) ";
     
         $create_post_query = mysqli_query($connection, $query);
 
