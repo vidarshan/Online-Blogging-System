6b22819236f7919f1a86079539e04e8c1f2f8bdf
vidarshanadithya98 <vidarshanadithya3@gmail.com>
2019-12-07 22:40:24 +0530

    Creating user operation pages and links on admin

--- admin/includes/admin_navigation.php
+++ admin/includes/admin_navigation.php
@@ -63,10 +63,10 @@
                         <a href="javascript:;" data-toggle="collapse" data-target="#demo"><i class="fa fa-fw fa-arrows-v"></i> Users <i class="fa fa-fw fa-caret-down"></i></a>
                         <ul id="demo" class="collapse">
                             <li>
-                                <a href="#">View All Users</a>
+                                <a href="./users.php">View All Users</a>
                             </li>
                             <li>
-                                <a href="#">Add Users</a>
+                                <a href="users.php?source=add_user">Add Users</a>
                             </li>
                         </ul>
                     </li>
