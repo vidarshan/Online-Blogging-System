f843c93ea2eba0bd474f6393176ef6e627fc8276
vidarshanadithya98 <vidarshanadithya3@gmail.com>
2019-12-07 11:57:03 +0530

    Added comment form on front end

--- post.php
+++ post.php
@@ -52,14 +52,31 @@
             
             ?>
 
+
+                <?php
+                    if(isset($_POST['create_comment'])){
+
+                        echo $_POST['comment_author'];
+
+                    }
+                ?>
                 <!-- Comments Form -->
                 <div class="well">
                     <h4>Leave a Comment:</h4>
-                    <form role="form">
-                        <div class="form-group">
-                            <textarea class="form-control" rows="3"></textarea>
+                    <form role="form" method="post" action="">
+                    <div class="form-group">
+                    <label for="author">Comment Author</label>
+                            <input type="text" class="form-control" name="comment_author">
                         </div>
-                        <button type="submit" class="btn btn-primary">Submit</button>
+                        <div class="form-group">
+                        <label for="email">Comment Email</label>
+                            <input type="email" class="form-control" name="comment_email">
+                        </div>
+                        <div class="form-group">
+                            <label for="comment">Comment</label>
+                            <textarea name="comment_content" class="form-control" rows="3"></textarea>
+                        </div>
+                        <button type="submit" name="create_comment" class="btn btn-primary">Submit</button>
                     </form>
                 </div>
 
