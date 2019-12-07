8e0ed4ed7547c64206b60fad4196772f8e29ecdc
vidarshanadithya98 <vidarshanadithya3@gmail.com>
2019-12-07 12:30:47 +0530

    Approve and unapprove comments

--- admin/includes/admin_navigation.php
+++ admin/includes/admin_navigation.php
@@ -54,7 +54,7 @@
                         <a href="./categories.php"><i class="fa fa-fw fa-wrench"></i> Categories</a>
                     </li>
 
-                    <li class="active">
+                    <li>
 
                     <!--default case in the switch statement in comments.php-->
                         <a href="./comments.php"><i class="fa fa-fw fa-file"></i> Comments</a>
@@ -70,7 +70,7 @@
                             </li>
                         </ul>
                     </li>
-                    <li class="active">
+                    <li>
                         <a href="blank-page.html"><i class="fa fa-fw fa-file"></i> Profile</a>
                     </li>
 
--- admin/includes/view_all_comments.php
+++ admin/includes/view_all_comments.php
@@ -59,8 +59,8 @@
                                         
                                         
                                         echo "<td>{$comment_date}</td>";
-                                        echo "<td><a href='comments.php?source=edit_post&p_id{$comment_id}'>Approve</a></td>";
-                                        echo "<td><a href='comments.php?source=edit_post&p_id{$comment_id}'>Unapprove</a></td>";
+                                        echo "<td><a href='comments.php?approve=$comment_id'>Approve</a></td>";
+                                        echo "<td><a href='comments.php?unapprove=$comment_id'>Unapprove</a></td>";
                                         echo "<td><a href='comments.php?delete={$comment_id}'>DELETE</a></td>";
 
 
@@ -68,6 +68,33 @@
 
                                     }
 
+                                    if(isset($_GET['unapprove'])){
+
+                                        $the_comment_id = $_GET['unapprove'];
+
+                                    $query = "UPDATE comments SET comment_status = 'unapproved' WHERE comment_id = $the_comment_id ";
+
+                                        $unapprove_comment_query = mysqli_query($connection, $query);
+
+                                        confirmQuery($unapprove_comment_query);
+
+                                        header("Location: ./comments.php");
+
+                                    }
+
+                                    if(isset($_GET['approve'])){
+
+                                        $the_comment_id = $_GET['approve'];
+
+                                    $query = "UPDATE comments SET comment_status = 'approved' WHERE comment_id = $the_comment_id ";
+
+                                        $approve_comment_query = mysqli_query($connection, $query);
+
+                                        confirmQuery($approve_comment_query);
+
+                                        header("Location: ./comments.php");
+
+                                    }
 
                                     if(isset($_GET['delete'])){
 
