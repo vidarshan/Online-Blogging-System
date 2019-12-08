ad910e461a66c10157bd2104f1e81cf0eb9b567a
vidarshanadithya98 <vidarshanadithya3@gmail.com>
2019-12-08 23:19:27 +0530

    Basic login functionality added

--- includes/login.php
+++ includes/login.php
@@ -4,8 +4,29 @@
 
 if (isset($_POST['login'])) {
 
-    echo $username = $_POST['username'];
-    echo $password = $_POST['password'];
+     $username = $_POST['username'];
+     $password = $_POST['password'];
+
+    $username = mysqli_real_escape_string($connection, $username);
+    $password = mysqli_real_escape_string($connection, $password);
+
+
+    $query = "SELECT * FROM users WHERE username = '{$username}' ";
+    $select_user_query = mysqli_query($connection, $query);
+
+    if(!$select_user_query){
+
+        die("Query failed ". mysqli_error($connection));
+
+    }
+
+
+    while($row = mysqli_fetch_array($select_user_query)){
+
+        echo $db_id = $row['user_id'];
+    }
+
+
 }
 
 ?>
