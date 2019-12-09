a2260fcc91961b83537bb7eee71dc04b794d3a83
vidarshanadithya98 <vidarshanadithya3@gmail.com>
2019-12-09 11:47:39 +0530

    Pulling user data for profile page

--- admin/profile.php
+++ admin/profile.php
@@ -3,7 +3,24 @@
 <?php
     if(isset($_SESSION['username'])){
         
+       $username =  $_SESSION['username'];
 
+         $query = "SELECT * FROM users WHERE username = '{$username}' ";
+
+         $select_user_profile_query = mysqli_query($connection, $query);
+
+         while($row = mysqli_fetch_array($select_user_profile_query)){
+
+            $user_id = $row['user_id'];
+            $username = $row['username'];
+            $user_password = $row['user_password'];
+            $user_firstname= $row['user_firstname'];
+            $user_lastname = $row['user_lastname'];
+            $user_email = $row['user_email'];
+           // $user_image = $row['user_image'];
+            $user_role = $row['user_role'];
+            //$user_date = $row['user_date'];
+         }
 
     }
 ?>
@@ -99,7 +116,7 @@
 
     <div class="form-group">
         
-    <input type="submit" value="Update User" name="edit_user" class="btn btn-primary ">
+    <input type="submit" value="Update Profile" name="update_profile" class="btn btn-primary ">
             
     </div>
 
