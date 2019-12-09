15cdd842555e07dfe40a07a47662800be7ed6220
vidarshanadithya98 <vidarshanadithya3@gmail.com>
2019-12-09 22:29:10 +0530

    Shortcut link to view post

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -138,6 +138,7 @@
                                         echo "<td>{$post_date}</td>";
                                         //posts.php file contains the conditions
                                         //standard and professional way
+                                        echo "<td><a href='../post.php?p_id={$post_id}'>VIEW POST</a></td>";
                                         echo "<td><a href='posts.php?source=edit_post&p_id={$post_id}'>EDIT</a></td>";
                                         echo "<td><a href='posts.php?delete={$post_id}'>DELETE</a></td>";
                                         echo "<tr>";
