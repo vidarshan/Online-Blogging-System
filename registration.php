e9fdbbed5a4984f52f77f6afbbb590ae79c827dc
vidarshanadithya98 <vidarshanadithya3@gmail.com>
2019-12-09 22:47:26 +0530

    Testing registration form

--- registration.php
+++ registration.php
@@ -1,6 +1,11 @@
 <!-- <?php  //include "includes/db.php"; ?> -->
  <?php  include "includes/header.php"; ?>
 
+ <?php
+    if(isset($_POST['submit'])){
+        
+    }
+ ?>
 
     <!-- Navigation -->
     
