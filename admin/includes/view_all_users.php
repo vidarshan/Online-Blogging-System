fcc56ced1165a87244ec5fcb737db590712177cb
vidarshanadithya98 <vidarshanadithya3@gmail.com>
2019-12-12 13:32:00 +0530

    MySQL Injection Protection

--- admin/includes/view_all_users.php
+++ admin/includes/view_all_users.php
@@ -55,7 +55,11 @@
     
                                                 if(isset($_GET['delete'])){
 
-                                                    $the_user_id = $_GET['delete'];
+                                                    if(isset($_SESSION['user_role'])){
+
+                                                        if($_SESSION['user_role'] == 'admin'){
+
+                                                    $the_user_id = mysqli_real_escape_string($connection,$_GET['delete']);
 
                                                     $query = "DELETE FROM users WHERE user_id = {$the_user_id} ";
 
@@ -64,6 +68,9 @@
                                                     confirmQuery($delete_user_query);
 
                                                     header("Location: ./users.php");
+                                                        }
+                                                    
+                                                    }
 
                                                 }
 
